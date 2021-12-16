c9a5f9023e4cb9aa84bf8559d6e3ff6ec7f8f65e
Mustafa Afandi <tapaafandi@gmail.com>
2021-12-16 11:11:51 +0700

    shared viewmodel

--- app/src/main/java/com/tapaafandi/todoappjetpackcompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/ui/viewmodels/SharedViewModel.kt
@@ -1,6 +1,29 @@
 package com.tapaafandi.todoappjetpackcompose.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tapaafandi.todoappjetpackcompose.data.models.ToDoTask
+import com.tapaafandi.todoappjetpackcompose.data.repositories.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SharedViewModel(): ViewModel() {
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val repository: ToDoRepository
+): ViewModel() {
+
+    private val _allTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
+    val allTasks: StateFlow<List<ToDoTask>> = _allTasks
+
+    fun getAllTasks(){
+        viewModelScope.launch {
+            repository.getAllTask.collect {
+                _allTasks.value = it
+            }
+        }
+    }
 }
