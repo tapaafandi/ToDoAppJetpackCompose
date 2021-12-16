fec053cb0d7be1f7ec47be5e2af3f9e54dfac7c4
Mustafa Afandi <tapaafandi@gmail.com>
2021-12-16 14:55:53 +0700

    top app bar

---
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/ui/screens/list/ListAppBar.kt
@@ -0,0 +1,33 @@
+package com.tapaafandi.todoappjetpackcompose.ui.screens.list
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tapaafandi.todoappjetpackcompose.ui.theme.topAppBarBackgroundColor
+import com.tapaafandi.todoappjetpackcompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun ListAppBar() {
+    DefaultListAppBar()
+}
+
+@Composable
+fun DefaultListAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Tasks",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
+    )
+}
+
+@Preview
+@Composable
+fun DefaultListAppBarPreview() {
+    DefaultListAppBar()
+}
