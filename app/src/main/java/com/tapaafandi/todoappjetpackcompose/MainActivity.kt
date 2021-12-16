587fed6e040b46625e445293d93be9c9b8d3c2a2
Mustafa Afandi <tapaafandi@gmail.com>
2021-12-16 10:45:05 +0700

    setup dagger hilt

--- app/src/main/java/com/tapaafandi/todoappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/MainActivity.kt
@@ -9,30 +9,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.tapaafandi.todoappjetpackcompose.ui.theme.ToDoAppJetpackComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoAppJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoAppJetpackComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package com.tapaafandi.todoappjetpackcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application()
