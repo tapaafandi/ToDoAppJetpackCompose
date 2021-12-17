d1eb6ce0626fb06481074eaf40a0f64d6a10fc34
Mustafa Afandi <tapaafandi@gmail.com>
2021-12-17 10:49:55 +0700

    search bar

--- app/src/main/java/com/tapaafandi/todoappjetpackcompose/components/PriorityItem.kt
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/components/PriorityItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.tapaafandi.todoappjetpackcompose.data.models.Priority
 import com.tapaafandi.todoappjetpackcompose.ui.theme.LARGE_PADDING
 import com.tapaafandi.todoappjetpackcompose.ui.theme.PRIORITY_INDICATOR_SIZE
--- app/src/main/java/com/tapaafandi/todoappjetpackcompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/tapaafandi/todoappjetpackcompose/ui/screens/list/ListAppBar.kt
@@ -1,8 +1,13 @@
 package com.tapaafandi.todoappjetpackcompose.ui.screens.list
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -10,16 +15,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import com.tapaafandi.todoappjetpackcompose.ui.theme.topAppBarBackgroundColor
-import com.tapaafandi.todoappjetpackcompose.ui.theme.topAppBarContentColor
 import com.tapaafandi.todoappjetpackcompose.R
 import com.tapaafandi.todoappjetpackcompose.components.PriorityItem
 import com.tapaafandi.todoappjetpackcompose.data.models.Priority
-import com.tapaafandi.todoappjetpackcompose.ui.theme.LARGE_PADDING
-import com.tapaafandi.todoappjetpackcompose.ui.theme.Typography
+import com.tapaafandi.todoappjetpackcompose.ui.theme.*
 
 @Composable
 fun ListAppBar() {
@@ -170,6 +176,83 @@ fun DeleteAllAction(
     }
 }
 
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBackgroundColor
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium),
+                    text = "Search",
+                    color = Color.White
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier.alpha(ContentAlpha.disabled),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        onCloseClicked()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon",
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colors.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent
+            )
+        )
+    }
+}
+
 @Preview
 @Composable
 fun DefaultListAppBarPreview() {
@@ -177,5 +260,16 @@ fun DefaultListAppBarPreview() {
         onSearchClicked = {},
         onSortClicked = {},
         onDeleteClicked = {}
+    )
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onCloseClicked = { /*TODO*/ },
+        onSearchClicked = {}
     )
 }
